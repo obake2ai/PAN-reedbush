cc6bd2d9bc179554f26a37b3ae74783e5dd740d1
yuma <fableooon@gmail.com>
2018-12-26 22:54:28 +0900

    add celeba

--- otherGANs/dataset.py
+++ otherGANs/dataset.py
@@ -72,9 +72,21 @@ class Dataset(nn.Module):
         dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batch_size, shuffle=True)
         return dataloader
 
+    def celeba(self, opt):
+        dataset = datasets.ImageFolder(root="./data/img_align_celeba/",
+                            transform=transforms.Compose([
+                                transforms.Resize(opt.img_size),
+                                transforms.CenterCrop(opt.img_size),
+                                transforms.ToTensor(),
+                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                            ]))
+        dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batch_size, shuffle=True)
+        return dataloader
+
 def makeDataloader(opt):
     dataset = Dataset(opt)
     if opt.dataset == 'mnist': return dataset.mnist(opt)
     if opt.dataset == 'cifar10': return dataset.cifar10(opt)
     if opt.dataset == 'fashion': return dataset.fashion(opt)
     if opt.dataset == 'lsun': return dataset.lsun(opt)
+    if opt.dataset == 'celeba': return dataset.celeba(opt)
