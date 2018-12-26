f0b5563c901afeba3356df15821ef27d5f564498
yuma <fableooon@gmail.com>
2018-12-26 18:11:05 +0900

    no message

--- models.py
+++ models.py
@@ -76,6 +76,32 @@ class NoiseGeneratorSimple(nn.Module):
         img = img.view(img.shape[0], *self.img_shape)
         return img
 
+class NoiseGeneratorSimple(nn.Module):
+    def __init__(self, opt):
+        super(NoiseGeneratorSimple, self).__init__()
+        if opt.dataset == 'mnist' or opt.dataset == 'fashion':
+          channels = 1
+        else:
+          channels = 3
+        self.img_shape = (channels, opt.img_size, opt.img_size)
+        def block(in_feat, out_feat, level, normalize=True):
+            layers = [AlgorithmicNoiseLayer(in_feat, out_feat, level, normalize)]
+            return layers
+
+        self.model = nn.Sequential(
+            *block(opt.latent_dim, 128, 0.1, normalize=False),
+            *block(128, 256, 0.1),
+            *block(256, 512, 0.1),
+            *block(512, 1024, 0.1),
+            nn.Linear(1024, int(np.prod(self.img_shape))),
+            nn.Tanh()
+        )
+
+    def forward(self, z):
+        img = self.model(z)
+        img = img.view(img.shape[0], *self.img_shape)
+        return img
+
 class NoiseGeneratorComplex(nn.Module):
     def __init__(self, opt):
         super(NoiseGeneratorComplex, self).__init__()
--- train_cifar10.py
+++ train_cifar10.py
@@ -27,7 +27,7 @@ opt = easydict.EasyDict({
 dataloader = dataset.makeDataloader(opt)
 
 # Initialize generator and discriminator
-generator = models.NoiseGeneratorSimple(opt)
+generator = models.ArgNoiseGeneratorSimple(opt)
 discriminator = models.DiscriminatorConv(opt)
 
 train(generator, discriminator, dataloader, opt)
