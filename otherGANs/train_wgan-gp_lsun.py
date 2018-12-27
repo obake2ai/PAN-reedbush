34f61f948abc5a07f79c13e1dd069506cb3e13aa
yuma <fableooon@gmail.com>
2018-12-27 19:39:21 +0900

    no message

--- otherGANs/train_wgan-gp_lsun.py
+++ otherGANs/train_wgan-gp_lsun.py
@@ -13,7 +13,7 @@ opt = easydict.EasyDict({
     'n_cpu': 8,
     'latent_dim': 128,
     'img_size': 64,
-    'n_critic': 1,
+    'n_critic': 5,
     'clip_value': 0.01,
     'sample_interval': 100,
     'log_interval': 10,
