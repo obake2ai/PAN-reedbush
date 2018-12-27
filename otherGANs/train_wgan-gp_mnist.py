a54017b937f28d0d8babb608df3b1ff7cea437f7
yuma <fableooon@gmail.com>
2018-12-27 19:35:36 +0900

    no message

--- otherGANs/train_wgan-gp_mnist.py
+++ otherGANs/train_wgan-gp_mnist.py
@@ -6,14 +6,14 @@ import easydict
 
 opt = easydict.EasyDict({
     'n_epochs': 200,
-    'batch_size': 64,
+    'batch_size': 128,
     'lr': 0.0002,
     'b1': 0.5,
     'b2': 0.999,
     'n_cpu': 8,
     'latent_dim': 128,
     'img_size': 32,
-    'n_critic': 1,
+    'n_critic': 5,
     'clip_value': 0.01,
     'sample_interval': 100,
     'log_interval': 10,
