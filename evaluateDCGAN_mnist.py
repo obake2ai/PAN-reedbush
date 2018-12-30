006e53dd5dba48607c0f964fd561ef963075cdf5
yuma <fableooon@gmail.com>
2018-12-30 18:45:54 +0900

    no message

---
+++ evaluateDCGAN_mnist.py
@@ -0,0 +1,10 @@
+import easydict
+from otherGANs import past_models
+from otherGANs import train_dcgan_mnist
+import calcIS
+
+opt = train_dcgan_mnist.opt
+opt.loadDir = './otherGANs/pass/DCGAN_mnist'
+
+generator = past_models.DCGANGenerator32(opt)
+calcIS.main(opt ,generator)
