b8c62728e19b821afaca2352f79ecfc49c2ffd9c
yuma <fableooon@gmail.com>
2018-12-30 18:52:19 +0900

    no message

--- evaluateDCGAN_mnist.py
+++ evaluateDCGAN_mnist.py
@@ -1,12 +1,27 @@
-import sys
-sys.path.append('./otherGANS')
 import easydict
 from otherGANs import past_models
 from otherGANs import train_dcgan_mnist
 import calcIS
 
-opt = train_dcgan_mnist.opt
-opt.loadDir = './otherGANs/pass/DCGAN_mnist'
+opt = easydict.EasyDict({
+    'n_epochs': 200,
+    'batch_size': 128,
+    'lr': 0.0002,
+    'b1': 0.5,
+    'b2': 0.999,
+    'n_cpu': 8,
+    'latent_dim': 128,
+    'img_size': 32,
+    'n_critic': 1,
+    'clip_value': 0.01,
+    'sample_interval': 100,
+    'log_interval': 10,
+    'dataset': 'mnist',
+    'num_filters': 128, #for CNN Discriminator and Generator
+    'saveDir' : None,
+    'resume' : None,
+    'loadDir' : './otherGANs/pass/DCGAN_mnist'
+})
 
 generator = past_models.DCGANGenerator32(opt)
 calcIS.main(opt ,generator)
