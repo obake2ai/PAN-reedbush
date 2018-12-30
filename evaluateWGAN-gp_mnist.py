e1da2b5cf718d08cf9ec0342e316171e2a83ba45
yuma <fableooon@gmail.com>
2018-12-30 19:00:02 +0900

    add eval files

---
+++ evaluateWGAN-gp_mnist.py
@@ -0,0 +1,26 @@
+import easydict
+from otherGANs import past_models
+import calcIS
+
+opt = easydict.EasyDict({
+    'n_epochs': 200,
+    'batch_size': 64,
+    'lr': 0.0002,
+    'b1': 0.5,
+    'b2': 0.999,
+    'n_cpu': 8,
+    'latent_dim': 128,
+    'img_size': 32,
+    'n_critic': 5,
+    'clip_value': 0.01,
+    'sample_interval': 100,
+    'log_interval': 10,
+    'dataset': 'mnist',
+    'num_filters': 128, #for CNN Discriminator and Generator
+    'saveDir' : None,
+    'resume' : None,
+    'loadDir' : './otherGANs/pass/WGAN-gp_mnist'
+})
+
+generator = past_models.DCGANGenerator32(opt)
+calcIS.main(opt ,generator)
