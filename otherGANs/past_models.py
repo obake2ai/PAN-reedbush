29d3e5aae0e53eaf2706fe9fac949e5969eaf22b
yuma <fableooon@gmail.com>
2018-12-31 19:19:44 +0900

    no message

--- otherGANs/past_models.py
+++ otherGANs/past_models.py
@@ -229,7 +229,7 @@ class DCGANDiscriminator32_BN(nn.Module):
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*4) x 8 x 8
             # nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 8),
+            # nn.BatchNorm2d(ndf * 8),
             # nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*8) x 4 x 4
             nn.Conv2d(ndf * 4, 1, 4, 1, 0, bias=False),
