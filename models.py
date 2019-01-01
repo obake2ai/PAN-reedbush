8dd47a5f84227d2cf44c7315e4c33a4a90258749
yuma <fableooon@gmail.com>
2019-01-01 16:35:54 +0900

    add resGW

--- models.py
+++ models.py
@@ -459,3 +459,34 @@ class NoiseResGenerator(nn.Module):
         img = self.model(z)
         img = img.view(img.shape[0], *self.img_shape)
         return img
+
+    class NoiseResGeneratorW(nn.Module):
+        def __init__(self, opt):
+            super(NoiseResGeneratorW, self).__init__()
+            if opt.dataset == 'mnist' or opt.dataset == 'fashion':
+              channels = 1
+            else:
+              channels = 3
+            self.img_shape = (channels, opt.img_size, opt.img_size)
+            def block(in_feat, out_feat, level, normalize=True):
+                layers = [NoiseLayer(in_feat, out_feat, level, normalize)]
+                return layers
+            def resblock(in_feat, out_feat, level, normalize=True):
+                layers = [NoiseBasicBlock(in_feat, out_feat, stride=1, shortcut=None, level=level, normalize=normalize)]
+                return layers
+
+            self.model = nn.Sequential(
+                *block(opt.latent_dim, 128, 0.1, normalize=False),
+                *resblock(128, 128, 0.1),
+                *block(128, 1024, 0.1),
+                *resblock(1024, 1024, 0.1),
+                *block(1024, 5012, 0.1),
+                *resblock(5012, 5012, 0.1),
+                nn.Linear(5012, int(np.prod(self.img_shape))),
+                nn.Tanh()
+            )
+
+        def forward(self, z):
+            img = self.model(z)
+            img = img.view(img.shape[0], *self.img_shape)
+            return img
