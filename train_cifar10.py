f3ce1f6e92be277d45b3380ef8792b480a8a76a7
yuma <fableooon@gmail.com>
2019-01-02 20:20:04 +0900

    no message

--- train_cifar10.py
+++ train_cifar10.py
@@ -30,9 +30,9 @@ opt = easydict.EasyDict({
 _, dataloader = dataset.makeDataloader(opt)
 
 # Initialize generator and discriminator
-generator = models.NoiseResGeneratorEcoLongB(opt)
+generator = models.NoiseResGeneratorEcoLongA(opt)
 nc = 3
-discriminator = naiveresnet.NoiseResNetEco32(naiveresnet.NoiseBasicBlock, [2,2,2,2], nchannels=nc, nfilters=opt.num_filters, nclasses=1, pool=2, level=0.1)
+discriminator = naiveresnet.NoiseResNet32(naiveresnet.NoiseBasicBlock, [2,2,2,2], nchannels=nc, nfilters=opt.num_filters, nclasses=1, pool=2, level=0.1)
 #discriminator = past_models.DCGANDiscriminator32_(opt)
 #discriminator = past_models.DCGANDiscriminator32_(opt)
 
