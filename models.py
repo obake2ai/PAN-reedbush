98180f17287a10868abaf763ff378c028e0b7cd3
yuma <fableooon@gmail.com>
2019-01-02 20:59:39 +0900

    no message

--- models.py
+++ models.py
@@ -417,6 +417,51 @@ class NoiseResGeneratorEcoLongB(nn.Module):
         img = img.view(img.shape[0], *self.img_shape)
         return img
 
+class NoiseResGeneratorEcoLongC(nn.Module):
+    def __init__(self, opt):
+        super(NoiseResGeneratorEcoLongC, self).__init__()
+        if opt.dataset == 'mnist' or opt.dataset == 'fashion':
+          channels = 1
+        else:
+          channels = 3
+        self.img_shape = (channels, opt.img_size, opt.img_size)
+        def block(in_feat, out_feat, level, normalize=True):
+            layers = [NoiseLayer(in_feat, out_feat, level, normalize)]
+            return layers
+        def resblock(in_feat, out_feat, level, normalize=True):
+            layers = [NoiseBasicBlock(in_feat, out_feat, stride=1, shortcut=None, level=level, normalize=normalize)]
+            return layers
+
+        self.model = nn.Sequential(
+            *block(opt.latent_dim, 128, 0.1, normalize=False),
+            *resblock(128, 128, 0.1),
+            *resblock(128, 128, 0.1),
+            *block(128, 256, 0.1),
+            *resblock(256, 256, 0.1),
+            *resblock(256, 256, 0.1),
+            *resblock(256, 256, 0.1),
+            *block(256, 512, 0.1),
+            *resblock(512, 512, 0.1),
+            *resblock(512, 512, 0.1),
+            *resblock(512, 512, 0.1),
+            *resblock(512, 512, 0.1),
+            *block(512, 1024, 0.1),
+            *resblock(1024, 1024, 0.1),
+            *resblock(1024, 1024, 0.1),
+            *resblock(1024, 1024, 0.1),
+            *resblock(1024, 1024, 0.1),
+            *resblock(1024, 1024, 0.1),
+            *resblock(1024, 1024, 0.1),
+            *resblock(1024, 1024, 0.1),
+            *block(1024,int(np.prod(self.img_shape)), 0.1),
+            nn.Tanh()
+        )
+
+    def forward(self, z):
+        img = self.model(z)
+        img = img.view(img.shape[0], *self.img_shape)
+        return img
+
 class ArgNoiseResGenerator(nn.Module):
     def __init__(self, opt):
         super(ArgNoiseResGenerator, self).__init__()
