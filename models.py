528ab405f500490fb875999dd826ad0f984e6166
yuma <fableooon@gmail.com>
2019-01-03 17:15:34 +0900

    no message

--- models.py
+++ models.py
@@ -390,7 +390,6 @@ class NoiseResGeneratorHead(nn.Module):
         else:
           channels = 3
         self.img_shape = (channels, opt.img_size, opt.img_size)
-        self.pre_layer = nn.Linear(opt.latent_dim, 256)
         def block(in_feat, out_feat, level, normalize=True):
             layers = [NoiseLayer(in_feat, out_feat, level, normalize)]
             return layers
@@ -399,6 +398,7 @@ class NoiseResGeneratorHead(nn.Module):
             return layers
 
         self.model = nn.Sequential(
+            nn.Linear(opt.latent_dim, 256),
             *block(256, 512, 0.1),
             *resblock(512, 512, 0.1),
             *block(512, 1024, 0.1),
@@ -410,9 +410,40 @@ class NoiseResGeneratorHead(nn.Module):
         )
 
     def forward(self, z):
-        x1 = self.pre_layer(z)
-        x2 = self.model(x1)
-        img = x2.view(x2.shape[0], *self.img_shape)
+        img = self.model(z)
+        img = img.view(img.shape[0], *self.img_shape)
+        return img
+
+class NoiseResGeneratorHead(nn.Module):
+    def __init__(self, opt):
+        super(NoiseResGeneratorHead, self).__init__()
+        if opt.dataset == 'mnist' or opt.dataset == 'fashion':
+          channels = 1
+        else:
+          channels = 3
+        self.img_shape = (channels, opt.img_size, opt.img_size)
+        def block(in_feat, out_feat, level, normalize=True):
+            layers = [NoiseLayer(in_feat, out_feat, level, normalize)]
+            return layers
+        def resblock(in_feat, out_feat, level, normalize=True):
+            layers = [NoiseBasicBlock(in_feat, out_feat, stride=1, shortcut=None, level=level, normalize=normalize)]
+            return layers
+
+        self.model = nn.Sequential(
+            nn.Linear(opt.latent_dim, 256),
+            *block(256, 512, 0.1),
+            *resblock(512, 512, 0.1),
+            *block(512, 1024, 0.1),
+            *resblock(1024, 1024, 0.1),
+            *resblock(1024, 1024, 0.1),
+            *resblock(1024, 1024, 0.1),
+            *block(1024,int(np.prod(self.img_shape)), 0.1),
+            nn.Tanh()
+        )
+
+    def forward(self, z):
+        img = self.model(z)
+        img = img.view(img.shape[0], *self.img_shape)
         return img
 
 class NoiseResGeneratorEcoLongB(nn.Module):
