9e762c45ea357a5a35367b5a0c9d19c1f1e8b3cf
yuma <fableooon@gmail.com>
2019-01-03 11:34:43 +0900

    no message

--- evaluatePAN_cifar10.py
+++ evaluatePAN_cifar10.py
@@ -19,8 +19,8 @@ opt = easydict.EasyDict({
     'num_filters': 128, #for CNN Discriminator and Generator
     'saveDir' : None,
     'resume' : None,
-    'loadDir' : './pass/PAN_cifar10_NRG_D32_5'
+    'loadDir' : './pass/PAN_cifar10_NRGelA_NRD'
 })
 
-generator = models.NoiseResGenerator(opt)
+generator = models.NoiseResGeneratorEcoLongA(opt)
 calcIS.main(opt, generator)
