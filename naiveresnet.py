17328314df2185e0090b4d0813ec5312bf2e76c4
yuma <fableooon@gmail.com>
2019-01-07 19:37:17 +0900

    add argResD

--- naiveresnet.py
+++ naiveresnet.py
@@ -10,6 +10,8 @@ if torch.cuda.is_available():
 else:
     device = torch.device("cpu")
 
+import noise_layers
+
 
 def conv3x3(in_planes, out_planes, stride=1):
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
@@ -44,6 +46,29 @@ class NoiseBasicBlock(nn.Module):
             nn.BatchNorm2d(planes),
             nn.ReLU(True),
             NoiseLayer(planes, planes, level),
+            nn.BatchNorm2d(planes),
+        )
+        self.shortcut = shortcut
+
+    def forward(self, x):
+        residual = x
+        y = self.layers(x)
+        if self.shortcut:
+            residual = self.shortcut(x)
+        y += residual
+        y = F.relu(y)
+        return y
+
+class ArgNoiseBasicBlock(nn.Module):
+    expansion = 1
+    def __init__(self, in_planes, planes, stride=1, shortcut=None, seed=0, level=0.2):
+        super(NoiseBasicBlock, self).__init__()
+        self.layers = nn.Sequential(
+            noise_layers.AlgorithmicNoiseLayer(in_planes, planes, seed, level),
+            nn.MaxPool2d(stride, stride),
+            nn.BatchNorm2d(planes),
+            nn.ReLU(True),
+            noise_layers.AlgorithmicNoiseLayer(planes, planes, level),
             nn.BatchNorm2d(planes),
         )
         self.shortcut = shortcut
@@ -168,6 +193,48 @@ class NoiseResNet32(nn.Module):
         x8 = self.linear(x7)
         return x8
 
+class ArgNoiseResNet32(nn.Module):
+    def __init__(self, block, nblocks, nchannels, nfilters, nclasses, pool, seeds, level):
+        super(NoiseResNet32, self).__init__()
+        self.in_planes = nfilters
+        self.pre_layers = nn.Sequential(
+            nn.Conv2d(nchannels,nfilters,kernel_size=7,stride=2,padding=3,bias=False),
+            nn.BatchNorm2d(nfilters),
+            nn.ReLU(True),
+            nn.MaxPool2d(kernel_size=3,stride=2,padding=1)
+        )
+        self.layer1 = self._make_layer(block, 1*nfilters, nblocks[0], seed=seed[0], level=level)
+        self.layer2 = self._make_layer(block, 2*nfilters, nblocks[1], stride=2, seed=seed[1], level=level)
+        self.layer3 = self._make_layer(block, 4*nfilters, nblocks[2], stride=2, seed=seed[2], level=level)
+        #self.layer4 = self._make_layer(block, 8*nfilters, nblocks[3], stride=2, level=level)
+        self.avgpool = nn.AvgPool2d(pool, stride=1)
+        self.linear = nn.Linear(4*nfilters*block.expansion, nclasses)
+
+    def _make_layer(self, block, planes, nblocks, stride=1, seed=0, level=0.2):
+        shortcut = None
+        if stride != 1 or self.in_planes != planes * block.expansion:
+            shortcut = nn.Sequential(
+                nn.Conv2d(self.in_planes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+        layers = []
+        layers.append(block(self.in_planes, planes, stride, shortcut, seed=seed, level=level))
+        self.in_planes = planes * block.expansion
+        for i in range(1, nblocks):
+            layers.append(block(self.in_planes, planes, seed = seed, level=level))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x1 = self.pre_layers(x)
+        x2 = self.layer1(x1)
+        x3 = self.layer2(x2)
+        x5 = self.layer3(x3)
+        x6 = self.avgpool(x5)
+        x7 = x6.view(x6.size(0), -1)
+        x8 = self.linear(x7)
+        return x8
+
 class NoiseResNetEco32(nn.Module):
     def __init__(self, block, nblocks, nchannels, nfilters, nclasses, pool, level):
         super(NoiseResNetEco32, self).__init__()
@@ -214,6 +281,9 @@ def noiseresnet18(nchannels, nfilters, nclasses, pool=7, level=0.1):
 
 def noiseresnet18_32(nchannels, nfilters, nclasses, pool=7, level=0.1):
     return NoiseResNet32(NoiseBasicBlock, [2,2,2,2], nchannels=nchannels, nfilters=nfilters, nclasses=nclasses, pool=pool, level=level)
+
+def argnoiseresnet18_32(nchannels, nfilters, nclasses, pool=7, level=0.1):
+    return NoiseResNet32(NoiseBasicBlock, [2,2,2,2], nchannels=nchannels, nfilters=nfilters, nclasses=nclasses, pool=pool, seeds=[0,2,4,6], level=level)
 
 def noiseresnet34(nchannels, nfilters, nclasses, pool=7, level=0.1):
     return NoiseResNet(NoiseBasicBlock, [3,4,6,3], nchannels=nchannels, nfilters=nfilters, nclasses=nclasses, pool=pool, level=level)
