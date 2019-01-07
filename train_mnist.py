e082d385a5254ce9e34fa41d3ed3d2bb2ebbafcf
yuma <fableooon@gmail.com>
2019-01-07 18:51:17 +0900

    no message

--- train_mnist.py
+++ train_mnist.py
@@ -30,7 +30,7 @@ _, dataloader = dataset.makeDataloader(opt)
 
 # Initialize generator and discriminator
 generator = models.NoiseResGenerator(opt)
-discriminator = past_models.DCGANDiscriminator32_(opt)
-#discriminator = naiveresnet.NoiseResNet32(naiveresnet.NoiseBasicBlock, [2,2,2,2], nchannels=1, nfilters=opt.num_filters, nclasses=1, pool=2, level=0.1)
+#discriminator = past_models.DCGANDiscriminator32_(opt)
+discriminator = naiveresnet.NoiseResNet32(naiveresnet.NoiseBasicBlock, [2,2,2,2], nchannels=1, nfilters=opt.num_filters, nclasses=1, pool=2, level=0.1)
 
 train(generator, discriminator, dataloader, opt)
