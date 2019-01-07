8fd330e51aafc5eb6af3f558f2588964a75ceef8
yuma <fableooon@gmail.com>
2019-01-07 23:52:41 +0900

    add Sand2

--- models.py
+++ models.py
@@ -315,6 +315,38 @@ class NoiseResGeneratorSand(nn.Module):
         img = img.view(img.shape[0], *self.img_shape)
         return img
 
+class NoiseResGeneratorSand2(nn.Module):
+    def __init__(self, opt):
+        super(NoiseResGeneratorSand2, self).__init__()
+        if opt.dataset == 'mnist' or opt.dataset == 'fashion':
+          channels = 1
+        else:
+          channels = 3
+        self.img_shape = (channels, opt.img_size, opt.img_size)
+        def block(in_feat, out_feat, level, normalize=True):
+            layers = [NoiseLayer(in_feat, out_feat, level, normalize)]
+            return layers
+        def resblock(in_feat, out_feat, level, normalize=True):
+            layers = [NoiseBasicBlock(in_feat, out_feat, stride=1, shortcut=None, level=level, normalize=normalize)]
+            return layers
+
+        self.model = nn.Sequential(
+            nn.Linear(opt.latent_dim, 128),
+            *block(128, 256, 0.1),
+            *block(256, 512, 0.1),
+            *block(512, 1024, 0.1),
+            *resblock(1024, 1024, 0.1),
+            *block(1024, 2048, 0.1),
+            *resblock(2048, 2048, 0.1),
+            nn.Linear(2048, int(np.prod(self.img_shape))),
+            nn.Tanh()
+        )
+
+    def forward(self, z):
+        img = self.model(z)
+        img = img.view(img.shape[0], *self.img_shape)
+        return img
+
 class NoiseResGeneratorIntent(nn.Module):
     def __init__(self, opt):
         super(NoiseResGeneratorIntent, self).__init__()
