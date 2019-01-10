9e6005ed48b4a7b7a373c67ef9c2e76eb67ff168
yuma <fableooon@gmail.com>
2019-01-11 08:02:13 +0900

    add model

--- models.py
+++ models.py
@@ -1027,6 +1027,35 @@ class NoiseGenerator2Dv6(nn.Module):
         img = self.model(x.view(-1, 128 * 8, 4, 4))
         return img
 
+class NoiseGenerator2Dv7(nn.Module):
+    def __init__(self, opt):
+        super(NoiseGenerator2Dv7, self).__init__()
+        channels = 1 if opt.dataset == 'mnist' or opt.dataset == 'fashion' else 3
+        self.pre_layer = nn.Linear(opt.latent_dim, 128 * 8 * 2 * 2)
+
+        self.model = nn.Sequential(
+            NoiseBasicBlock2D(128 * 8, 128 * 8, level=0.1),
+            NoiseLayer2D(128 * 8, 128 * 4, 0.1),
+            nn.Upsample(scale_factor=2, mode='bilinear'), #(2, 2) -> (4, 4)
+            NoiseBasicBlock2D(128 * 6, 128 * 6, level=0.1),
+            NoiseLayer2D(128 * 6, 128 * 4, 0.1),
+            nn.Upsample(scale_factor=2, mode='bilinear'), #(4, 4) -> (8, 8)
+            NoiseBasicBlock2D(128 * 4, 128 * 4, level=0.1),
+            NoiseLayer2D(128 * 4, 128 * 2, 0.1),
+            nn.Upsample(scale_factor=2, mode='bilinear'), #(8, 8) -> (16, 16)
+            NoiseBasicBlock2D(128 * 2, 128 * 2, level=0.1),
+            NoiseLayer2D(128 * 2, 128 * 1, 0.1),
+            nn.Upsample(scale_factor=2, mode='bilinear'), #(16, 16) -> (32, 32)
+            NoiseBasicBlock2D(128 * 1, 128 * 1, level=0.1),
+            NoiseLayer2D(128 * 1, channels, 0.1),
+            nn.Tanh()
+        )
+
+    def forward(self, z):
+        x = self.pre_layer(z)
+        img = self.model(x.view(-1, 128 * 8, 2, 2))
+        return img
+
 class NoiseGenerator(nn.Module):
     def __init__(self, opt):
         super(NoiseGenerator, self).__init__()
