2c9f8f72aabd1384fe807aba276ab7491cc4dac0
yuma <fableooon@gmail.com>
2019-01-12 00:23:31 +0900

    bug fix

--- naiveresnet.py
+++ naiveresnet.py
@@ -114,7 +114,7 @@ class LCGNoiseBasicBlock_(nn.Module):
             nn.MaxPool2d(stride, stride),
             nn.BatchNorm2d(planes),
             nn.ReLU(True),
-            noise_layers.LCGNoiseLayer2D_(planes, planes, level, seed+1, size=[size[0], int(size[1]/stride), int(size[2]/stride])),
+            noise_layers.LCGNoiseLayer2D_(planes, planes, level, seed+1, size=[size[0], int(size[1]/stride), int(size[2]/stride)]),
             nn.BatchNorm2d(planes),
         )
         self.shortcut = shortcut
