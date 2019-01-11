5bc0480c5e5ccefa979b3e846b42fc2ecb8502cd
yuma <fableooon@gmail.com>
2019-01-11 23:55:46 +0900

    bug fix

--- train_cifar10_nolog.py
+++ train_cifar10_nolog.py
@@ -33,7 +33,7 @@ _, dataloader = dataset.makeDataloader(opt)
 # Initialize generator and discriminator
 generator = models.LCGNoiseGenerator2Dv6_(opt)
 #discriminator = naiveresnet.NoiseResNet32(naiveresnet.NoiseBasicBlock, [2,2,2,2], nchannels=3, nfilters=opt.num_filters, nclasses=1, pool=2, level=0.1)
-discriminator = naiveresnet.LCGNoiseResNet32_(naiveresnet.LCGNoiseBasicBlock_, [2,2,2,2], nchannels=3, nfilters=opt.num_filters, nclasses=1, pool=2, level=0.1, seeds=[100, 200, 300], sizes=[8, 4, 2])
+discriminator = naiveresnet.LCGNoiseResNet32_(naiveresnet.LCGNoiseBasicBlock_, [2,2,2,2], nchannels=3, nfilters=opt.num_filters, nclasses=1, pool=2, level=0.1, seeds=[100, 200, 300], sizes=[8, 8, 4])
 #discriminator = past_models.WGANDiscriminator32_(opt)
 #discriminator = past_models.DCGANDiscriminator32_(opt)
 
