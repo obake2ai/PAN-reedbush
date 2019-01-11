c5c867bc10678508c328ab55beda5f62b648d625
yuma <fableooon@gmail.com>
2019-01-11 10:44:19 +0900

    model fix

--- models.py
+++ models.py
@@ -977,17 +977,15 @@ class NoiseGenerator2Dv5(nn.Module):
 
         self.model = nn.Sequential(
             NoiseLayer2D(opt.latent_dim, 128 * 8, 0.1),
-            NoiseLayer2D(128 * 8, 128 * 4, 0.1),
             nn.Upsample(scale_factor=2, mode='bilinear'), #(1, 1) -> (2, 2)
-            NoiseLayer2D(128 * 4, 128 * 4, 0.1),
+            NoiseLayer2D(128 * 8, 128 * 6, 0.1),
             nn.Upsample(scale_factor=2, mode='bilinear'), #(2, 2) -> (4, 4)
-            NoiseLayer2D(128 * 4, 128 * 2, 0.1),
+            NoiseLayer2D(128 * 6, 128 * 4, 0.1),
             nn.Upsample(scale_factor=2, mode='bilinear'), #(4, 4) -> (8, 8)
-            NoiseLayer2D(128 * 2, 128 * 2, 0.1),
+            NoiseLayer2D(128 * 4, 128 * 2, 0.1),
             nn.Upsample(scale_factor=2, mode='bilinear'), #(8, 8) -> (16, 16)
             NoiseLayer2D(128 * 2, 128 * 1, 0.1),
             nn.Upsample(scale_factor=2, mode='bilinear'), #(16, 16) -> (32, 32)
-            NoiseLayer2D(128 * 1, 128 * 1, 0.1),
             NoiseLayer2D(128 * 1, channels, 0.1),
             nn.Tanh()
         )
