376eacd3a715add82215eee2ea52b7e404b5214e
yuma <fableooon@gmail.com>
2019-01-11 16:28:49 +0900

    add MTDiscriminator

--- naiveresnet.py
+++ naiveresnet.py
@@ -36,6 +36,31 @@ class NoiseLayer(nn.Module):
         z = self.layers(y)
         return z
 
+class MTNoiseLayer(nn.Module):
+    def __init__(self, in_planes, out_planes, level, seed, normalize=True):
+        super(MTNoiseLayer, self).__init__()
+
+        self.level = level
+        self.seed = seed
+        if normalize:
+            self.layers = nn.Sequential(
+                nn.ReLU(True),
+                nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=1),
+                nn.BatchNorm2d(out_planes),
+            )
+        else:
+            self.layers = nn.Sequential(
+                nn.ReLU(True),
+                nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=1),
+            )
+
+    def forward(self, x):
+        torch.manual_seed(self.seed)
+        x.data += torch.rand(x.size(1), x.size(2), x.size(3)).cuda() * self.level
+
+        x = self.layers(x)
+        return x
+
 class NoiseBasicBlock(nn.Module):
     expansion = 1
     def __init__(self, in_planes, planes, stride=1, shortcut=None, level=0.2):
@@ -46,6 +71,29 @@ class NoiseBasicBlock(nn.Module):
             nn.BatchNorm2d(planes),
             nn.ReLU(True),
             NoiseLayer(planes, planes, level),
+            nn.BatchNorm2d(planes),
+        )
+        self.shortcut = shortcut
+
+    def forward(self, x):
+        residual = x
+        y = self.layers(x)
+        if self.shortcut:
+            residual = self.shortcut(x)
+        y += residual
+        y = F.relu(y)
+        return y
+
+class MTNoiseBasicBlock(nn.Module):
+    expansion = 1
+    def __init__(self, in_planes, planes, stride=1, shortcut=None, seed=0, level=0.2):
+        super(MTNoiseBasicBlock, self).__init__()
+        self.layers = nn.Sequential(
+            NoiseLayer(in_planes, planes, level, seed),
+            nn.MaxPool2d(stride, stride),
+            nn.BatchNorm2d(planes),
+            nn.ReLU(True),
+            NoiseLayer(planes, planes, level, seed+1),
             nn.BatchNorm2d(planes),
         )
         self.shortcut = shortcut
@@ -181,6 +229,48 @@ class NoiseResNet32(nn.Module):
         self.in_planes = planes * block.expansion
         for i in range(1, nblocks):
             layers.append(block(self.in_planes, planes, level=level))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x1 = self.pre_layers(x)
+        x2 = self.layer1(x1)
+        x3 = self.layer2(x2)
+        x5 = self.layer3(x3)
+        x6 = self.avgpool(x5)
+        x7 = x6.view(x6.size(0), -1)
+        x8 = self.linear(x7)
+        return x8
+
+class MTNoiseResNet32(nn.Module):
+    def __init__(self, block, nblocks, nchannels, nfilters, nclasses, pool, level, seeds):
+        super(MTNoiseResNet32, self).__init__()
+        self.in_planes = nfilters
+        self.pre_layers = nn.Sequential(
+            nn.Conv2d(nchannels,nfilters,kernel_size=7,stride=2,padding=3,bias=False),
+            nn.BatchNorm2d(nfilters),
+            nn.ReLU(True),
+            nn.MaxPool2d(kernel_size=3,stride=2,padding=1)
+        )
+        self.layer1 = self._make_layer(block, 1*nfilters, nblocks[0], level=level, seed=seeds[0])
+        self.layer2 = self._make_layer(block, 2*nfilters, nblocks[1], stride=2, level=level, seed=seeds[1])
+        self.layer3 = self._make_layer(block, 4*nfilters, nblocks[2], stride=2, level=level, seed=seeds[2])
+        #self.layer4 = self._make_layer(block, 8*nfilters, nblocks[3], stride=2, level=level)
+        self.avgpool = nn.AvgPool2d(pool, stride=1)
+        self.linear = nn.Linear(4*nfilters*block.expansion, nclasses)
+
+    def _make_layer(self, block, planes, nblocks, stride=1, level=0.2, seed=0):
+        shortcut = None
+        if stride != 1 or self.in_planes != planes * block.expansion:
+            shortcut = nn.Sequential(
+                nn.Conv2d(self.in_planes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+        layers = []
+        layers.append(block(self.in_planes, planes, stride, shortcut, level=level, seed=seed))
+        self.in_planes = planes * block.expansion
+        for i in range(1, nblocks):
+            layers.append(block(self.in_planes, planes, level=level, seed=seed))
         return nn.Sequential(*layers)
 
     def forward(self, x):
--- train_celeba.py
+++ train_celeba.py
@@ -31,9 +31,9 @@ opt = easydict.EasyDict({
 _, dataloader = dataset.makeDataloader(opt)
 
 # Initialize generator and discriminator
-generator = models.NoiseGenerator2Dv6(opt)
-#discriminator = naiveresnet.NoiseResNet32(naiveresnet.NoiseBasicBlock, [2,2,2,2], nchannels=3, nfilters=opt.num_filters, nclasses=1, pool=2, level=0.1)
-discriminator = past_models.WGANDiscriminator32_(opt)
+generator = models.MTNoiseGenerator2Dv6(opt)
+discriminator = naiveresnet.MTNoiseResNet32(naiveresnet.NoiseBasicBlock, [2,2,2,2], nchannels=3, nfilters=opt.num_filters, nclasses=1, pool=2, level=0.1)
+#discriminator = past_models.WGANDiscriminator32_(opt)
 #discriminator = past_models.DCGANDiscriminator32_(opt)
 
 train(generator, discriminator, dataloader, opt)
