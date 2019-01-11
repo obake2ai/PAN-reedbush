0f9c7ac98772f393c9602af75bc8b902f3cdcbf8
yuma <fableooon@gmail.com>
2019-01-12 00:01:17 +0900

    debug

--- noise_layers.py
+++ noise_layers.py
@@ -241,6 +241,8 @@ class LCGNoiseLayer2D_(nn.Module):
             )
 
     def forward(self, x):
+        print (x.size())
+        print (self.noise.size())
         x2 = torch.add(x, self.noise)
         z = self.layers(x2)
         return z
