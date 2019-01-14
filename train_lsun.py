36944d7c56f204039e1c8a5bcce8da86687d5124
yuma <fableooon@gmail.com>
2019-01-14 17:18:55 +0900

    cond

--- train_lsun.py
+++ train_lsun.py
@@ -24,21 +24,21 @@ opt = easydict.EasyDict({
     'dataset': 'lsun',
     'num_filters': 128, #for CNN Discriminator and Generator
     'saveDir' : None,
-    'resume' : 206100, #0 = train from scratch
+    'resume' : 0, #0 = train from scratch
     'logIS' : True,
-    'loadDir' : '1138:190113_NoiseGenerator2Dv6_NoiseResNet32_lsun'
+    'loadDir' : False
 })
 
 _, dataloader = dataset.makeDataloader(opt)
 
 # Initialize generator and discriminator
-generator = models.NoiseGenerator2Dv6(opt)
-#generator = models.MTNoiseGenerator2Dv6(opt)
+#generator = models.NoiseGenerator2Dv6(opt)
+generator = models.MTNoiseGenerator2Dv6(opt, seed=0)
 #generator = models.LCGNoiseGenerator2Dv6_(opt, seed=0)
 #generator = models.NoiseResGenerator2Dv6(opt, noise_layers.NoiseBasicBlock2Dv2, [2,2,2,2,1], level=0.1)
-discriminator = naiveresnet.NoiseResNet32(naiveresnet.NoiseBasicBlock, [2,2,2,2], nchannels=3, nfilters=opt.num_filters, nclasses=1, pool=2, level=0.1)
+#discriminator = naiveresnet.NoiseResNet32(naiveresnet.NoiseBasicBlock, [2,2,2,2], nchannels=3, nfilters=opt.num_filters, nclasses=1, pool=2, level=0.1)
 #discriminator = naiveresnet.LCGNoiseResNet32_(naiveresnet.LCGNoiseBasicBlock_, [2,2,2,2], nchannels=3, nfilters=opt.num_filters, nclasses=1, pool=2, level=0.1, seeds=[100, 200, 300], sizes=[8, 8, 4])
-#discriminator = naiveresnet.MTNoiseResNet32(naiveresnet.MTNoiseBasicBlock, [2,2,2,2], nchannels=3, nfilters=opt.num_filters, nclasses=1, pool=2, level=0.1, seeds=[100, 200, 300])
+discriminator = naiveresnet.MTNoiseResNet32(naiveresnet.MTNoiseBasicBlock, [2,2,2,2], nchannels=3, nfilters=opt.num_filters, nclasses=1, pool=2, level=0.1, seeds=[100, 200, 300])
 #discriminator = naiveresnet.NoiseResNet32(naiveresnet.NoiseBasicBlock, [2,2,2,2], nchannels=3, nfilters=opt.num_filters, nclasses=1, pool=2, level=0.1)
 #discriminator = past_models.WGANDiscriminator32_(opt)
 #discriminator = past_models.DCGANDiscriminator32_(opt)
