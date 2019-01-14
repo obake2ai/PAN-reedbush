c03bfed9552f922fa65e2785884051cb389dc536
yuma <fableooon@gmail.com>
2019-01-14 17:22:21 +0900

    cond

--- train_lsun.py
+++ train_lsun.py
@@ -24,9 +24,9 @@ opt = easydict.EasyDict({
     'dataset': 'lsun',
     'num_filters': 128, #for CNN Discriminator and Generator
     'saveDir' : None,
-    'resume' : 0, #0 = train from scratch
+    'resume' : 214900, #0 = train from scratch
     'logIS' : True,
-    'loadDir' : False
+    'loadDir' : '1137:190113_NoiseGenerator2Dv6_WGANDiscriminator32__lsun'
 })
 
 _, dataloader = dataset.makeDataloader(opt)
@@ -38,9 +38,9 @@ generator = models.MTNoiseGenerator2Dv6(opt, seed=0)
 #generator = models.NoiseResGenerator2Dv6(opt, noise_layers.NoiseBasicBlock2Dv2, [2,2,2,2,1], level=0.1)
 #discriminator = naiveresnet.NoiseResNet32(naiveresnet.NoiseBasicBlock, [2,2,2,2], nchannels=3, nfilters=opt.num_filters, nclasses=1, pool=2, level=0.1)
 #discriminator = naiveresnet.LCGNoiseResNet32_(naiveresnet.LCGNoiseBasicBlock_, [2,2,2,2], nchannels=3, nfilters=opt.num_filters, nclasses=1, pool=2, level=0.1, seeds=[100, 200, 300], sizes=[8, 8, 4])
-discriminator = naiveresnet.MTNoiseResNet32(naiveresnet.MTNoiseBasicBlock, [2,2,2,2], nchannels=3, nfilters=opt.num_filters, nclasses=1, pool=2, level=0.1, seeds=[100, 200, 300])
+#discriminator = naiveresnet.MTNoiseResNet32(naiveresnet.MTNoiseBasicBlock, [2,2,2,2], nchannels=3, nfilters=opt.num_filters, nclasses=1, pool=2, level=0.1, seeds=[100, 200, 300])
 #discriminator = naiveresnet.NoiseResNet32(naiveresnet.NoiseBasicBlock, [2,2,2,2], nchannels=3, nfilters=opt.num_filters, nclasses=1, pool=2, level=0.1)
-#discriminator = past_models.WGANDiscriminator32_(opt)
+discriminator = past_models.WGANDiscriminator32_(opt)
 #discriminator = past_models.DCGANDiscriminator32_(opt)
 
 train(generator, discriminator, dataloader, opt)
