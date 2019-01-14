c79b87d67bc9008e408b994520b8d9101b442e31
yuma <fableooon@gmail.com>
2019-01-14 17:56:50 +0900

    cond

--- train_cifar10.py
+++ train_cifar10.py
@@ -25,7 +25,7 @@ opt = easydict.EasyDict({
     'num_filters': 128, #for CNN Discriminator and Generator
     'saveDir' : None,
     'resume' : 0,
-    'logIS' : False,
+    'logIS' : True,
     'loadDir' : None
 })
 
