75c35110bb933a21519a120748854d5371293566
yuma <fableooon@gmail.com>
2019-01-16 01:30:11 +0900

    cond

--- train_celeba.py
+++ train_celeba.py
@@ -32,7 +32,7 @@ opt = easydict.EasyDict({
 _, dataloader = dataset.makeDataloader(opt)
 
 # Initialize generator and discriminator
-generator = models.NoiseGenerator2Dv7(opt)
+generator = models.NoiseGenerator2Dv8(opt)
 #generator = models.MTNoiseGenerator2Dv6(opt, seed=0)
 #generator = models.LCGNoiseGenerator2Dv6_(opt, seed=0)
 #generator = models.NoiseResGenerator2Dv1(opt, noise_layers.NoiseBasicBlock2Dv2, [2,2,2,2,1], level=0.1)
