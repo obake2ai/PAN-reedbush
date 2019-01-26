8f62de2927e6e8069138474311fe2c049f07c811
yuma <fableooon@gmail.com>
2019-01-26 12:24:04 +0900

    cond

--- train_shunga.py
+++ train_shunga.py
@@ -9,13 +9,13 @@ import easydict
 
 opt = easydict.EasyDict({
     'n_epochs': 200,
-    'batch_size': 8,
+    'batch_size': 32,
     'lr': 0.0002,
     'b1': 0.5,
     'b2': 0.999,
     'n_cpu': 8,
     'latent_dim': 128,
-    'img_size': 512,
+    'img_size': 1024,
     'n_critic': 1,
     'clip_value': 0.01,
     'sample_interval': 100,
