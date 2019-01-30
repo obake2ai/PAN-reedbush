87dea080c2c606b64683d4a846abced99f9658ed
yuma <fableooon@gmail.com>
2019-01-30 23:44:52 +0900

    cond

--- train_maico.py
+++ train_maico.py
@@ -9,13 +9,13 @@ import easydict
 
 opt = easydict.EasyDict({
     'n_epochs': 2000,
-    'batch_size': 8,
+    'batch_size': 32,
     'lr': 0.0002,
     'b1': 0.5,
     'b2': 0.999,
     'n_cpu': 8,
     'latent_dim': 128,
-    'img_size': 1024,
+    'img_size': 512,
     'n_critic': 1,
     'clip_value': 0.01,
     'sample_interval': 100,
@@ -24,23 +24,23 @@ opt = easydict.EasyDict({
     'dataset': 'maico2kiku',
     'num_filters': 128, #for CNN Discriminator and Generator
     'saveDir' : None,
-    'resume' : 9300, #0 = train from scratch
+    'resume' : 51000, #0 = train from scratch
     'logIS' : False,
-    'loadDir' : '1449:190125_MTNoiseGenerator2Dv6SND1024_x4v2_MTNoiseResNet1024_maico2kiku'
+    'loadDir' : '1754:190125_MTNoiseGenerator2Dv6SND512_MTNoiseResNet512_maico2kiku'
 })
 
 _, dataloader = dataset.makeDataloader(opt)
 
 # Initialize generator and discriminator
 #generator = models.NoiseGenerator2Dv6(opt)
-#generator = models.MTNoiseGenerator2Dv6SND512(opt, seed=0)
-generator = models.MTNoiseGenerator2Dv6SND1024_x4v2(opt, seed=0)
+generator = models.MTNoiseGenerator2Dv6SND512(opt, seed=0)
+#generator = models.MTNoiseGenerator2Dv6SND1024_x4v2(opt, seed=0)
 #generator = models.MTNoiseGenerator2Dv6(opt, seed=40)
 #generator = models.LCGNoiseGenerator2Dv6_(opt, seed=6)
 #generator = models.NoiseResGenerator2Dv1(opt, noise_layers.NoiseBasicBlock2Dv2, [2,2,2,2,1], level=0.1)
 #discriminator = naiveresnet.NoiseResNet32(naiveresnet.NoiseBasicBlock, [2,2,2,2], nchannels=3, nfilters=opt.num_filters, nclasses=1, pool=2, level=0.1)
-#discriminator = naiveresnet.MTNoiseResNet512(naiveresnet.MTSNDNoiseBasicBlock, [1,1,1,1,1,1,1,1], nchannels=3, nfilters=opt.num_filters, nclasses=1, pool=2, level=0.1, seed=0)
-discriminator = naiveresnet.MTNoiseResNet1024(naiveresnet.MTSNDNoiseBasicBlock, [1,1,1,1,1,1,1,1], nchannels=3, nfilters=opt.num_filters, nclasses=1, pool=2, level=0.1, seed=0)
+discriminator = naiveresnet.MTNoiseResNet512(naiveresnet.MTSNDNoiseBasicBlock, [1,1,1,1,1,1,1,1], nchannels=3, nfilters=opt.num_filters, nclasses=1, pool=2, level=0.1, seed=0)
+#discriminator = naiveresnet.MTNoiseResNet1024(naiveresnet.MTSNDNoiseBasicBlock, [1,1,1,1,1,1,1,1], nchannels=3, nfilters=opt.num_filters, nclasses=1, pool=2, level=0.1, seed=0)
 #discriminator = naiveresnet.LCGNoiseResNet32_(naiveresnet.LCGNoiseBasicBlock_, [2,2,2,2], nchannels=3, nfilters=opt.num_filters, nclasses=1, pool=2, level=0.1, seeds=[100, 200, 300], sizes=[8, 8, 4])
 #discriminator = naiveresnet.MTNoiseResNet32(naiveresnet.MTNoiseBasicBlock, [2,2,2,2], nchannels=3, nfilters=opt.num_filters, nclasses=1, pool=2, level=0.1, seeds=[10, 20,90])
 #discriminator = naiveresnet.LCGNoiseResNet32_(naiveresnet.LCGNoiseBasicBlock_, [2,2,2,2], nchannels=3, nfilters=opt.num_filters, nclasses=1, pool=2, level=0.1, seeds=[7, 8, 9], sizes=[8, 8, 4])
