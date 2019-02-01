56807e89d788a95a922b0c394193d879a8b1aff2
yuma <fableooon@gmail.com>
2019-02-01 16:03:40 +0900

    bug fix

--- generateVideo.py
+++ generateVideo.py
@@ -8,15 +8,14 @@ cuda = True if torch.cuda.is_available() else False
 Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
 
 def generate_interpolation_video(generator, opt, image_shrink=1, image_zoom=1, duration_sec=60.0, smoothing_sec=1.0, mp4=None, mp4_fps=30, mp4_codec='libx265', mp4_bitrate='16M', random_seed=1000, minibatch_size=8):
-    network_model = os.path.join(opt.loadDir, "generator_model_%s") % str(opt.resume).zfill(8)
-    print (network_model)
+    network_model = os.path.join(opt.loadDir, "generator_model_%s") % str(opt.resume - opt.resume%opt.modelsave_interval).zfill(8)
     if mp4 is None:
         mp4 = network_model + '-lerp.mp4'
     num_frames = int(np.rint(duration_sec * mp4_fps))
     random_state = np.random.RandomState(random_seed)
 
     print('Loading network from "%s"...' % network_model)
-    generator.load_state_dict(torch.load(network_model))
+    generator.load_state_dict(torch.load(os.path.join(opt.loadDir, "generator_model_%s") % str(opt.resume - opt.resume%opt.modelsave_interval).zfill(8)))
 
     print('Generating latent vectors...')
     shape = [num_frames, opt.latent_dim] # [frame, component]
