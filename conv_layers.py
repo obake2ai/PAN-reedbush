2fcc5e98f977ffdd9327c6bd05054e5253e2e121
yuma <fableooon@gmail.com>
2019-02-06 19:46:22 +0900

    add conv mix model

---
+++ conv_layers.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+
+import itertools
+
+class TransConvLayer(nn.Module):
+    def __init__(self, in_planes, out_planes):
+        super(TransConvLayer, self).__init__()
+
+        self.layers = nn.Sequential(
+            nn.ConvTranspose2d(in_planes, out_planes, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(out_planes),
+            nn.ReLU(True),
+        )
+
+    def forward(self, x):
+        y = self.layers(x)
+        return y
