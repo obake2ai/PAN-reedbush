e13cab3130c523462f66b1dc7d01e715b48c5173
yuma <fableooon@gmail.com>
2019-02-10 15:58:26 +0900

    add model

--- models.py
+++ models.py
@@ -1908,6 +1908,38 @@ class NoiseConvGenerator2Dv8_512(nn.Module):
         img = self.model(x.view(-1, 128 * 8, 4, 4))
         return img
 
+class NoiseConvGenerator2Dv9_512(nn.Module):
+    def __init__(self, opt, seed=None):
+        super(NoiseConvGenerator2Dv9_512, self).__init__()
+        channels = 1 if opt.dataset == 'mnist' or opt.dataset == 'fashion' else 3
+        self.img_shape = (channels, opt.img_size, opt.img_size)
+
+        self.pre_layer = nn.Linear(opt.latent_dim, 128 * 8 * 4 * 4)
+
+        self.model = nn.Sequential(
+            MTSNDNoiseLayer2Dx(128 * 8, 128 * 8, 0.1, seed=seed),
+            ConvUpsample(128 * 8, 128 * 8), #(4, 4) -> (8, 8)
+            MTSNDNoiseLayer2Dx(128 * 8, 128 * 4, 0.1, seed=seed+10),
+            ConvUpsample(128 * 4, 128 * 4),  #(8, 8) -> (16, 16)
+            MTSNDNoiseLayer2Dx(128 * 4, 128 * 2, 0.1, seed=seed+20),
+            ConvUpsample(128 * 2, 128 * 2), #(16, 16) -> (32, 32)
+            MTSNDNoiseLayer2Dx(128 * 2, 128 * 1, 0.1, seed=seed+30),
+            ConvUpsample(128 * 1, 128 * 1), #(32, 32) -> (64, 64)
+            MTSNDNoiseBasicBlock2Dx(128 * 1, 128 * 1, 0.1, seed=seed+40),
+            ConvUpsample(128 * 1, 128 * 1), #(64, 64) -> (128, 128)
+            MTSNDNoiseBasicBlock2Dx(128 * 1, 128 * 1, 0.1, seed=seed+50),
+            ConvUpsample(128 * 1, 128 * 1), #(128, 128) -> (256, 256)
+            MTSNDNoiseBasicBlock2Dx(128 * 1, 128 * 1, 0.1, seed=seed+60),
+            ConvUpsample(128 * 1, 128 * 1),             #(256, 256) -> (512, 512)
+            MTSNDNoiseLayer2Dx(128 * 1, channels, 0.1, seed=seed+70),
+            nn.Tanh()
+        )
+
+    def forward(self, z):
+        x = self.pre_layer(z)
+        img = self.model(x.view(-1, 128 * 8, 4, 4))
+        return img
+
 class NoiseGenerator(nn.Module):
     def __init__(self, opt):
         super(NoiseGenerator, self).__init__()
