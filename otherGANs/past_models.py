896b49535669befbc7cf2366f2b4e46e92c48cc3
yuma <fableooon@gmail.com>
2019-02-11 16:09:56 +0900

    model fix

--- otherGANs/past_models.py
+++ otherGANs/past_models.py
@@ -288,19 +288,19 @@ class WGANDiscriminator512_(nn.Module):
             nn.BatchNorm2d(ndf * 2),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*8) x 64 x 64
-            nn.Conv2d(ndf * 2, ndf * 4, 4, 1, 0, bias=False),
+            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 4),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*8) x 32 x 32
-            nn.Conv2d(ndf * 4, ndf * 4, 4, 1, 0, bias=False),
+            nn.Conv2d(ndf * 4, ndf * 4, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 4),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*8) x 16 x 16
-            nn.Conv2d(ndf * 4, ndf * 8, 4, 1, 0, bias=False),
+            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 8),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*8) x 8 x 8
-            nn.Conv2d(ndf * 8, ndf * 8, 4, 1, 0, bias=False),
+            nn.Conv2d(ndf * 8, ndf * 8, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 8),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*8) x 4 x 4
